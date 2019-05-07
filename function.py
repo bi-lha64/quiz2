1fd7aed278961d025e256149d4d8ffe94c76529b
wanjiku <wanjikubilha64@gmail.com>
2019-05-07 19:25:53 +0300

    add new

---
+++ function.py
@@ -0,0 +1,54 @@
+def tens():
+	list[1,2,3,4,5,6,7,8,9]
+	for t in list:
+		z=t*10
+		print(z)
+
+def squares():
+	list[2,4,6,8,10,12]	
+	for t in list:
+		return[t**2]
+
+def sorted():
+	a[9,1,4,7,3]
+	b[5,2,6,8,0]
+	c=sorted()
+
+def range_sum():
+
+
+
+
+
+
+
+
+def largest():
+	max([1,3,6,8,2,4,5.7])
+	print(max(8))
+	
+
+def smallest():
+	min([1,3,6,2,4,5.7])
+	print(min(1))
+
+
+def my_mudulus():
+	d=dict()
+	for x in range(10,20):
+		d[x]=%3
+		print(d)
+
+
+def list_students():
+	students=[{'balance':1000,'name':'irene'}]
+	students=[{'balance':2000,'name':'pauline'}]
+	students=[{'balance':3000,'name':'rose'}]
+	print("hello {} your is{} .formart(name,balance));
+	print("hello {} your is{} .formart(name,balance));
+	print("hello {} your is{} .formart(name,balance));
+		
+
+
+
+
